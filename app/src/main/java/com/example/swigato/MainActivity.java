8719681cd459d5977d3ac56836e6dbf7cb863a17
brahmjot <brahmjot.nagpal@gmail.com>
2023-04-17 15:57:12 +0530

    inital commit

---
+++ app/src/main/java/com/example/swigato/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.swigato;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView txtSnacks=findViewById(R.id.txtSnacks);
+        TextView txtMainCourse=findViewById(R.id.txtMainCourse);
+        TextView txtDesert = findViewById(R.id.txtDesert);
+
+        txtSnacks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Snacks.class);
+                startActivity(intent);
+
+            }
+        });
+
+        txtMainCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,MainCourse.class);
+                startActivity(intent);
+
+            }
+        });
+
+        txtDesert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,Desert.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+    }
+}
